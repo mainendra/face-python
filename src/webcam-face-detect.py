2bb142c7cb0f97a245c2772ba50e25f6421c6d97
Mainendra <1897340+mainendra@users.noreply.github.com>
2020-09-12 00:22:47 -0600

    Flip webcam image

--- src/webcam-face-detect.py
+++ src/webcam-face-detect.py
@@ -7,13 +7,14 @@ webcam = cv2.VideoCapture(0)
 # Keep reading fram from webcam
 while True:
     success, frame = webcam.read()
+    img_flip = cv2.flip(frame, 1)
     # Convert frame to gray scale
-    grayImg = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    grayImg = cv2.cvtColor(img_flip, cv2.COLOR_BGR2GRAY)
     # Draw rectangle on matched face
     for (x, y, w, h) in face_cascade.detectMultiScale(grayImg):
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 5)
+        cv2.rectangle(img_flip, (x, y), (x + w, y + h), (0, 255, 0), 5)
     # Show frame
-    cv2.imshow("Video", frame)
+    cv2.imshow("Video", img_flip)
     # Wait for 1ms or `q` key press
     if cv2.waitKey(1) == ord('q'):
         break
